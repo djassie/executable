8d70685f0735ba0fc7b6ace591e23af1c4f0f64f
Susobhan Das <elitedsusobhan@gmail.com>
2022-11-26 01:46:18 +0530

    commit

---
+++ CMakeFiles/crud-exe.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for crud-exe.
+# This may be replaced when dependencies are built.
---
+++ CMakeFiles/crud-lib.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/crud-lib.dir/src/ErrorHandler.cpp.o"
+  "CMakeFiles/crud-lib.dir/src/ErrorHandler.cpp.o.d"
+  "CMakeFiles/crud-lib.dir/src/service/UserService.cpp.o"
+  "CMakeFiles/crud-lib.dir/src/service/UserService.cpp.o.d"
+  "libcrud-lib.a"
+  "libcrud-lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/crud-lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
