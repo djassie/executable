514cc6e0c99b4b7d9c42c176b2c91b2d2c823c55
Susobhan Das <88635857+djassie@users.noreply.github.com>
2022-11-26 01:47:34 +0530

    Delete CTestTestfile.cmake

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jacobd/Downloads/example-crud
-# Build directory: /home/jacobd/Downloads/example-crud/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(project-tests "crud-test")
-set_tests_properties(project-tests PROPERTIES  _BACKTRACE_TRIPLES "/home/jacobd/Downloads/example-crud/CMakeLists.txt;83;add_test;/home/jacobd/Downloads/example-crud/CMakeLists.txt;0;")
